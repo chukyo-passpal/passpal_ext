75750cd20b593f5c557811261534607219b256ed
okaren1017 <oka721017@icloud.com>
2025-08-05 12:12:30 +0900

    CU_IDパスワード入力画面 #13

---
+++ action/components/textButton.tsx
@@ -0,0 +1,13 @@
+interface TextButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+	children: React.ReactNode;
+}
+
+const TextButton: React.FC<TextButtonProps> = ({ children, ...props }) => {
+	return (
+		<button className="text-primary text-left font-[14px] font-medium cursor-pointer hover:underline" {...props}>
+			{children}
+		</button>
+	);
+};
+
+export default TextButton;
--- action/routes/auth/password.tsx
+++ action/routes/auth/password.tsx
@@ -1,12 +1,38 @@
-import { Link, createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import AuthHeader from "../../components/AuthHeader";
+import InputField from "../../components/InputField";
+import Button from "../../components/Button";
+import TextButton from "../../components/textButton";
+import { useState } from "react";
+import { Lock } from "lucide-react";
 
 const PasswordPage = () => {
+	const [password, setPassword] = useState("");
+	const navigate = useNavigate();
+
+	const handleOnClickButton = () => {
+		navigate({ to: "/auth/init-setting" });
+	};
+
+	const handleOnClickTextButton = () => {
+		navigate({ to: "/auth/student-id" });
+	};
+
 	return (
-		<div className="flex flex-col gap-4 p-4">
-			<h1>Password</h1>
-			<Link to="/auth/init-setting" className="bg-primary text-white rounded px-4 py-2 text-center">
-				初期設定
-			</Link>
+		<div className="w-full h-full flex flex-col gap-4">
+			<AuthHeader title="CU_IDのパスワードを入力" comment="t324016@m.chukyo-u.ac.jp" />
+			<InputField
+				icon={<Lock size={20} />}
+				label="パスワード"
+				type="password"
+				value={password}
+				onChange={(e) => setPassword(e.target.value)}
+				placeholder="パスワードを入力"
+			/>
+			<Button variant="primary" disabled={!password.trim()} onClick={handleOnClickButton}>
+				ログイン
+			</Button>
+			<TextButton onClick={handleOnClickTextButton}>学籍番号入力に戻る</TextButton>
 		</div>
 	);
 };
